08a5c445af356cd37f37694ab758dd692f85332d
hasti <hasti.kaviyan21poor@gmail.com>
2025-08-30 22:59:56 +0330

    sub sect

--- src/components/subscribe/Subscribe.jsx
+++ src/components/subscribe/Subscribe.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import Button1 from '../../section/Nav/button/button'
 
-const subscribe = () => {
+const Subscribe = () => {
   return (
-    <div>
-      subscribe
-    </div>
+
+    
+    <section
+      id="contact-us"
+      className="max-container flex justify-center items-center flex-col text-center py-10"
+    >
+      <h3 className="text-4xl leading-[68px] font-palanquin font-bold">
+        Sign up for
+        <span className="text-coral-red"> Updates </span>
+        & Newsletter
+      </h3>
+      <div className='mt-12 lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full '>
+          <input type='text' placeholder='subscribe@nike.com' className='input' />
+          <div className='flex max-sm:justify-end items-center max-sm:w-full'>
+            <Button1 label='Sign Up' fullWidth />
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default subscribe
+export default Subscribe
